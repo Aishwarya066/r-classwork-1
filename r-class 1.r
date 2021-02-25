7deb1e7ae5b9b7c940b97cf0dd3769750e1834e7
Aishwarya066 <79004902+Aishwarya066@users.noreply.github.com>
2021-02-25 16:07:59 +0530

    Create r-class 1.r

---
+++ r-class 1.r
@@ -0,0 +1,21 @@
+print("Hello World")
+2+8
+50000*84687
+7654/76
+(33+65)*7
+nummy<- c(2,3,4) 
+nummy_int<- c(1L,2L,3L)
+typeof(nummy)
+typeof(nummy_int)
+is.numeric(nummy)
+is.numeric(nummy_int)
+is.double(nummy)
+logicals<- c(TRUE, TRUE, T, FALSE)
+money_logical <- as.logical(money_money)
+month.abb
+month.abb[6]
+month.abb[c(2,5,7)]
+month.abb[c(6,8,10)]
+month.abb[c(6:12)]
+days<- c("Mon", "Wed", "Fri")
+days
